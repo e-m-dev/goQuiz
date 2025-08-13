3d3bd08b91f953200bfad81b7259814e627450fb
e-m-dev <eric.murray.dev@gmail.com>
2025-08-13 14:21:25 +0100

    feat: add CORS support to HTTP router for improved cross-origin requests

--- server/internal/http/router.go
+++ server/internal/http/router.go
@@ -1,13 +1,27 @@
 package http
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
 
 func NewRouter(h *Handler) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
 	r.Post("/rooms", h.CreateRoomHandler)
 	r.Get("/rooms/{code}", h.GetRoomHandler)
 	r.Post("/rooms/{code}/join", h.JoinRoomHandler)
 	r.Post("/rooms/{code}/leave", h.LeaveRoomHandler)
 	r.Get("/ws/{code}", h.WSHandler)
+
 	return r
 }
