495f5bdefe8152b815e5615b90257760a86dbd17
e-m-dev <eric.murray.dev@gmail.com>
2025-08-13 14:32:22 +0100

    refactor: remove unnecessary whitespace from FormCreateRoom component

--- frontend/src/components/FormCreateRoom.tsx
+++ frontend/src/components/FormCreateRoom.tsx
@@ -48,11 +48,6 @@ const FormCreateRoom: React.FC = () => {
                 <p className="font-normal text-gray-700 dark:text-gray-400">{roomCode}</p>
             </div>
         </div>
-
-
-        
-
-
     );
 };
 
