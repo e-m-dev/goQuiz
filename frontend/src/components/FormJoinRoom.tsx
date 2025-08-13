1dc7a8acc63094445193de413709b50c14766526
e-m-dev <eric.murray.dev@gmail.com>
2025-08-13 14:40:03 +0100

    feat: implement FormJoinRoom component with join room functionality

--- frontend/src/components/FormJoinRoom.tsx
+++ frontend/src/components/FormJoinRoom.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { joinRoom } from "../lib/api";
+
 const FormJoinRoom: React.FC = () => {
+    const [playerName, setPlayerName] = useState('');
+    const [roomCode, setRoomCode] = useState('');
+
+    const handleRoom = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRoomCode(event.target.value);
+    }
+
+    const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPlayerName(event.target.value);
+    }
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        try {
+            const data = await joinRoom(roomCode, playerName);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
-        <>
-        </>
+        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+                <h5 className="text-xl font-medium text-gray-900 dark:text-white">Join Room</h5>
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Room Code</label>
+                    <input type="text" value={roomCode} onChange={handleRoom} name="rName" id="rName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="XXXXXX" required />
+                </div>
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player Name</label>
+                    <input type="text" value={playerName} onChange={handleName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                </div>
+                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Room</button>
+            </form>
+        </div>
     );
 };
 
