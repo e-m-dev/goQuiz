68f3f626b8591a02a3e2e5da42d8b34a3a51f0fe
e-m-dev <eric.murray.dev@gmail.com>
2025-08-16 15:43:23 +0100

    feat: implement ActionButton component and integrate it into Landing

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,5 +1,6 @@
 import { useLocalStorage } from '@uidotdev/usehooks';
 import React from 'react';
+import ActionButton from './ActionButton';
 
 const Landing: React.FC = () => {
     const [name, saveName] = useLocalStorage("playerName", '');
@@ -8,13 +9,20 @@ const Landing: React.FC = () => {
         saveName(event.target.value);
     }
 
+    const navigate = (path: string) => {
+
+    }
+
     return (
         <form className="max-w-sm mx-auto">
             <div className="mb-5">
                 <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player Name</label>
                 <input type="text" id="base-input" value={name} onChange={handleName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
+            <ActionButton label='Create Room' onClick={() => navigate("/create")} />
+            <ActionButton label='Join Room' onClick={() => navigate("/join")} />
         </form>
+        
     );
 };
 
