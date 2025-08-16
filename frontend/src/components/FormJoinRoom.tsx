4a6e1cda6f4c553db82138f49eafcb89ae225dc7
e-m-dev <eric.murray.dev@gmail.com>
2025-08-16 16:32:02 +0100

    feat: refactor FormJoinRoom component to remove player name input and utilize localStorage for player name + save session info from backend resp

--- frontend/src/components/FormJoinRoom.tsx
+++ frontend/src/components/FormJoinRoom.tsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import { joinRoom } from "../lib/api";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 const FormJoinRoom: React.FC = () => {
-    const [playerName, setPlayerName] = useState('');
     const [roomCode, setRoomCode] = useState('');
+    const [session, setSession] = useLocalStorage("session", { roomCode: "", playerId: "" });
+    const playerName = localStorage.getItem("playerName") || '';
 
     const handleRoom = (event: React.ChangeEvent<HTMLInputElement>) => {
         setRoomCode(event.target.value);
-    }
-
-    const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPlayerName(event.target.value);
     }
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         try {
             const data = await joinRoom(roomCode, playerName);
+            setSession({ roomCode, playerId: data.id });
             console.log(data);
         } catch (error) {
             console.log(error);
@@ -29,10 +28,6 @@ const FormJoinRoom: React.FC = () => {
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Room Code</label>
                     <input type="text" value={roomCode} onChange={handleRoom} name="rName" id="rName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="XXXXXX" required />
-                </div>
-                <div>
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player Name</label>
-                    <input type="text" value={playerName} onChange={handleName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                 </div>
                 <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Room</button>
             </form>
