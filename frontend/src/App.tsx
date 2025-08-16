114880fa572ab4e80b4d57c519f85f4f023a584a
e-m-dev <eric.murray.dev@gmail.com>
2025-08-16 15:24:32 +0100

    feat: add Landing component for player name input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import FormCreateRoom from './components/FormCreateRoom'
 import FormJoinRoom from './components/FormJoinRoom'
+import Landing from './components/Landing'
 
 function App() {
 
   return (
     <div>
+      <Landing></Landing>
       <FormCreateRoom></FormCreateRoom>
       <FormJoinRoom></FormJoinRoom>
     </div>
