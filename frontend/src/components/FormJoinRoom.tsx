784d98748e339ea5f3a42c1c6cee822e85ec8107
e-m-dev <eric.murray.dev@gmail.com>
2025-08-18 13:28:20 +0100

    refactor: update FormJoinRoom component to use Card and Input for improved UI

--- frontend/src/components/FormJoinRoom.tsx
+++ frontend/src/components/FormJoinRoom.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { joinRoom } from "../lib/api";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "./ui/card";
 
 const FormJoinRoom: React.FC = () => {
     const [roomCode, setRoomCode] = useState('');
@@ -23,15 +25,23 @@ const FormJoinRoom: React.FC = () => {
         }
     }
     return(
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <form className="space-y-6" onSubmit={handleSubmit}>
-                <h5 className="text-xl font-medium text-gray-900 dark:text-white">Join Room</h5>
-                <div>
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Room Code</label>
-                    <input type="text" value={roomCode} onChange={handleRoom} name="rName" id="rName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="XXXXXX" required />
-                </div>
-                <Button type="submit" variant="outline">Join</Button>
-            </form>
+        <div className='min-h-dvh grid place-items-center p-4'>
+            <Card className='w-full max-w-sm'>
+                <CardHeader>
+                    <CardTitle>Join Game</CardTitle>
+                    <CardDescription>
+                        Enter the room code to join!
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form className="space-y-4" onSubmit={handleSubmit}>
+                        <Input type="text" value={roomCode} onChange={handleRoom} required />
+                        <div className='flex justify-center gap-2'>
+                            <Button type="submit">Join Room</Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 };
