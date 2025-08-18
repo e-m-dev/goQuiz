c18dddd2ef691ad130e693616b1a1c3f6b50079b
e-m-dev <eric.murray.dev@gmail.com>
2025-08-18 13:24:29 +0100

    refactor: restructure Landing component to use Card and Input for improved UI

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -2,6 +2,8 @@ import { useLocalStorage } from '@uidotdev/usehooks';
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
 import { Button } from './ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { Input } from './ui/input';
 
 const Landing: React.FC = () => {
     const [name, saveName] = useLocalStorage("playerName", '');
@@ -12,27 +14,37 @@ const Landing: React.FC = () => {
     }
 
     return (
-        <form className="max-w-sm mx-auto">
-            <div className="mb-5">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Player Name</label>
-                <input type="text" id="base-input" value={name} onChange={handleName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-            <Button onClick={() => {
-                if (!name.trim()) {
-                alert("Please enter your name first!");
-                return;
-                }
-                navigate("/create");
-            }}>Create Room</Button>
-            <Button onClick={() => {
-                if (!name.trim()) {
-                    alert("Please enter your name first!");
-                    return;
-                }
-                navigate("/join");
-            }}>Join Room</Button>
-        </form>
-        
+        <div className='min-h-dvh grid place-items-center p-4'>
+            <Card className='w-full max-w-sm'>
+                <CardHeader>
+                    <CardTitle>Player Name</CardTitle>
+                    <CardDescription>
+                        Your name will be displayed in game!
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form className="space-y-4">
+                        <Input type="text" value={name} onChange={handleName} required />
+                        <div className='flex justify-center gap-2'>
+                            <Button onClick={() => {
+                                if (!name.trim()) {
+                                alert("Please enter your name first!");
+                                return;
+                                }
+                                navigate("/create");
+                            }}>Create Room</Button>
+                            <Button onClick={() => {
+                                if (!name.trim()) {
+                                    alert("Please enter your name first!");
+                                    return;
+                                }
+                                navigate("/join");
+                            }}>Join Room</Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
     );
 };
 
