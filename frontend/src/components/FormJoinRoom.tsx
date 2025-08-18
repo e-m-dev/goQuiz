1658a202bb4c4d23d8f2e6201147f4c9b26b3f76
e-m-dev <eric.murray.dev@gmail.com>
2025-08-18 12:44:30 +0100

    fix: update button text from 'Create Room' to 'Join' in FormJoinRoom component

--- frontend/src/components/FormJoinRoom.tsx
+++ frontend/src/components/FormJoinRoom.tsx
@@ -29,7 +29,7 @@ const FormJoinRoom: React.FC = () => {
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Room Code</label>
                     <input type="text" value={roomCode} onChange={handleRoom} name="rName" id="rName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="XXXXXX" required />
                 </div>
-                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Room</button>
+                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Join</button>
             </form>
         </div>
     );
